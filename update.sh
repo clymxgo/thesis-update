e9ef5587a0e8eeb83b1e516315f8775f8f42efcc
clyxmgo <maxkz2409@gmail.com>
2025-05-30 14:37:48 +0500

    second step

--- update.sh
+++ update.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+#VARS section
 workdir=/opt/hafsdf
 updatefile=/tmp/fileupdate.zip
 backupdir=$HOME/backupdir
 
+#section thesis
+systemctl stop thesis-mobile.service
+systemctl stop thesis.service
+
+#save first string
+head -1 $workdir/bin/setenv.sh > $backupdir/setenv.sh
 
