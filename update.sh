504e4534fe085a2173a68fa90e62b15165aad6fa
clyxmgo <maxkz2409@gmail.com>
2025-05-30 19:59:44 +0500

    add missing directory

--- update.sh
+++ update.sh
@@ -7,10 +7,12 @@ backupdir=$HOME/backupfolder
 updatedir=/tmp/thesisupdate/tomcat
 
 echo "start update" > ./error.log
+echo "$backupdir" >> ./error.log
+echo $workdir >> ./error.log
 
 #section thesis
-sudo systemctl stop thesis-mobile.service
-sudo systemctl stop thesis.service
+#sudo systemctl stop thesis-mobile.service
+#sudo systemctl stop thesis.service
 
 #save first string
 head -1 $workdir/bin/setenv.sh > $backupdir/setenv.sh 2> ./error.log
@@ -30,7 +32,7 @@ rm $workdir/conf/*
 rm $workdir/conf/app_home/logback.xml 2>> ./error.log
 
 echo "extract zip file to temp folder" >> ./error.log
-unzip $updatefile -d /tmp/thesisupdate/ 2>> ./error.log
+unzip $updatefile -d /tmp/thesisupdate/ > ./unzip.log
 
 echo "copy folder from update" >> ./error.log
 sudo cp -r $updatedir/{bin,lib,shared,webapps} $workdir/ 2>> ./error.log
@@ -41,8 +43,15 @@ cp $updatedir/conf/app_home/logback.xml $workdir/conf/app_home/ 2>> ./error.log
 echo "cp index.html file" >> ./error.log
 sudo cp $backupdir/tomcat/webapps/ROOT/index.html $workdir/webapps/ROOT/ 2>> ./error.log
 
+#copy conf files
+sudo cp $updatedir/conf/* $workdir/conf/ 2>> ./error.log
+#copy app-hr-rest
+sudo cp -r $updatedir/conf/app-hr-rest $workdir/conf/ 2>> ./error.log
+
 echo "change mode files" >> ./error.log
 sudo chmod +x $workdir/bin/*.sh 2>> ./error.log
 
+echo "add string from file setenv.sh" >> error.log
+sudo { head -n 1 $backupdir/tomcat/bin/setenv.sh; cat $workdir/bin/setenv.sh; } > temp && mv temp $workdir/bin/setenv.sh
 exit 0
 
