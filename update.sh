59ce4d49e689ea44f94689907c74101f6fb95b7e
clyxmgo <maxkz2409@gmail.com>
2025-05-30 14:47:02 +0500

    step3

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 #VARS section
 workdir=/opt/hafsdf
 updatefile=/tmp/fileupdate.zip
-backupdir=$HOME/backupdir
+backupdir=$HOME/backupfolder
 
 #section thesis
 systemctl stop thesis-mobile.service
@@ -11,4 +11,15 @@ systemctl stop thesis.service
 
 #save first string
 head -1 $workdir/bin/setenv.sh > $backupdir/setenv.sh
+
+#copy workdir to backup folder
+cp -r $workdir $backupdir
+
+#create dump db
+#later
+
+#remove old folder at workdir
+rm -r $workdir/{bin,lib,shared,webapps}
+#remove files at tomcat/conf dir
+rm $workdir/conf/*
 
