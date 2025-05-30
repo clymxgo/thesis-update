fa6bde9599e851e9bf757ff78b069dab25c5ff52
clyxmgo <maxkz2409@gmail.com>
2025-05-30 15:04:05 +0500

    step4

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@
 workdir=/opt/hafsdf
 updatefile=/tmp/fileupdate.zip
 backupdir=$HOME/backupfolder
+updatedir=/tmp/thesisupdate/tomcat
 
 #section thesis
 systemctl stop thesis-mobile.service
@@ -22,4 +23,17 @@ cp -r $workdir $backupdir
 rm -r $workdir/{bin,lib,shared,webapps}
 #remove files at tomcat/conf dir
 rm $workdir/conf/*
+#remove logback.xml
+rm $workdir/conf/app_home/logback.xml
+
+#copy folder from update
+cp -r $updatedir/{bin,lib,shared,webapps} $workdir/
+#extract zip file to temp folder
+unzip $updatefile -d /tmp/thesisupdate/
+#copy new logback.xml
+cp $updatedir/conf/app_home/logback.xml $workdir/conf/app_home/
+#cp index.xml file
+cp $backupdir/tomcat/webapps/ROOT/index.xml $workdir/conf/webapps/ROOT/
+#change mode files
+chmod +x $workdir/bin/*.sh
 
