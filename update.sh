2d6096a9522c16e2fc575d809ee129c816b1a0b3
clyxmgo <maxkz2409@gmail.com>
2025-06-02 16:59:20 +0500

    additional changes

--- update.sh
+++ update.sh
@@ -1,57 +1,67 @@
 #!/bin/bash
 
 #VARS section
-workdir=/opt/haulmont/tomcat
-updatefile=/tmp/thesisupdate.zip
-backupdir=$HOME/backupfolder
-updatedir=/tmp/thesisupdate/tomcat
+WORKDIR=/opt/haulmont/tomcat
+UPDATEFILE=/tmp/thesisupdate.zip
+BACKUPDIR=$HOME/backupfolder
+UPDATEDIR=/tmp/thesisupdate/tomcat
 
-echo "start update" > ./error.log
-echo "$backupdir" >> ./error.log
-echo $workdir >> ./error.log
+echo "start update"
+echo "your backupdir= $BACKUPDIR" 
+echo "your workdir= $WORKDIR"
 
 #section thesis
-#sudo systemctl stop thesis-mobile.service
-#sudo systemctl stop thesis.service
 
-#save first string
+##uncomment next if you have a separate thesis-mobile service
+#sudo systemctl stop thesis-mobile.service
+
+#stop the main thesis service
+sudo systemctl stop thesis.service
+
+echo "get and save first string from file setenv.sh by instructions"
 head -1 $workdir/bin/setenv.sh > $backupdir/setenv.sh 2> ./error.log
 
 echo "copy workdir to backup folder" >> ./error.log
-cp -r $workdir $backupdir  2>> ./error.log
+cp -r $WORKDIR $BACKUPDIR  2>> ./error.log
 
 #create dump db
-#later
+#a little bit later)
 
-echo "remove old folder at workdir" >> ./error.log
-rm -rf $workdir/{bin,lib,shared,webapps}  2>> ./error.log
+echo "remove some folders by instuctions (bin,lib,shared,webapps)"
+sudo rm -rf $WORKDIR/{bin,lib,shared,webapps}  2>> ./error.log
 
-echo "remove files at tomcat/conf dir" >> ./error.log
-rm $workdir/conf/*
-#remove logback.xml
-rm $workdir/conf/app_home/logback.xml 2>> ./error.log
+echo "remove all files at tomcat/conf dir"
+sudo rm $WORKDIR/conf/*
 
-echo "extract zip file to temp folder" >> ./error.log
-unzip $updatefile -d /tmp/thesisupdate/ > ./unzip.log
+echo "remove logback.xml from app_home dir"
+sudo rm $WORKDIR/conf/app_home/logback.xml 2>> ./error.log
 
-echo "copy folder from update" >> ./error.log
-sudo cp -r $updatedir/{bin,lib,shared,webapps} $workdir/ 2>> ./error.log
+echo "extract update from zip file to temp folder /tmp/thesisupdate/"
+unzip $UPDATEFILE -d /tmp/thesisupdate/ > ./unzip.log
 
-#copy new logback.xml
-cp $updatedir/conf/app_home/logback.xml $workdir/conf/app_home/ 2>> ./error.log
+echo "copy {bin,lib,shared,webapps} folders to workdir"
+sudo cp -r $UPDATEDIR/{bin,lib,shared,webapps} $WORKDIR/ 2>> ./error.log
 
-echo "cp index.html file" >> ./error.log
-sudo cp $backupdir/tomcat/webapps/ROOT/index.html $workdir/webapps/ROOT/ 2>> ./error.log
+echo "copy new logback.xml to app_home"
+sudo cp $UPDATEDIR/conf/app_home/logback.xml $WORKDIR/conf/app_home/ 2>> ./error.log
 
-#copy conf files
-sudo cp $updatedir/conf/* $workdir/conf/ 2>> ./error.log
-#copy app-hr-rest
-sudo cp -r $updatedir/conf/app-hr-rest $workdir/conf/ 2>> ./error.log
+## uncomment next if you configuration was a redirection to short name
+#echo "restore index.html file if you have a redirection"
+#sudo cp $BACKUPDIR/tomcat/webapps/ROOT/index.html $WORKDIR/webapps/ROOT/ 2>> ./error.log
 
-echo "change mode files" >> ./error.log
-sudo chmod +x $workdir/bin/*.sh 2>> ./error.log
+echo "copy new files from 'conf' dir "
+sudo cp $UPDATEDIR/conf/* $WORKDIR/conf/ 2>> ./error.log
+
+echo "copy new folder app-hr-rest from update dir"
+sudo cp -r $UPDATEDIR/conf/app-hr-rest $WORKDIR/conf/ 2>> ./error.log
+
+echo "do executable all '.sh' files"
+sudo chmod +x $WORKDIR/bin/*.sh 2>> ./error.log
 
 echo "add string from file setenv.sh" >> error.log
-sudo { head -n 1 $backupdir/tomcat/bin/setenv.sh; cat $workdir/bin/setenv.sh; } > temp && mv temp $workdir/bin/setenv.sh
+{ head -n 1 $backupdir/tomcat/bin/setenv.sh; cat $workdir/bin/setenv.sh; } > temp && sudo mv temp $workdir/bin/setenv.sh 2>> error.log
+
+echo "done!"
 exit 0
+
 
